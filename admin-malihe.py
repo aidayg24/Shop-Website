4a28b139417603e9263a56103c67e15ee8cb1d41
malihemz <malihemirzaee44@gmail.com>
2020-11-19 22:06:39 +0330

    990829 update admin

--- admin-malihe.py
+++ admin-malihe.py
@@ -1,62 +1,70 @@
 class Admin:
-    list1 = []
 
     def __init__(self):
         self.passw = hash(13741387)
 
     def check_pass(self, ramz=None):
         while self.passw != ramz:
-            ramz = hash(int(input("ramz vared kon")))
+            ramz = hash(int(input("enter your password: ")))
             if self.passw == ramz:
-                print("ok")
-                new_ramz = input("behtar baraye amniat ramzeto avaz koni?yes/no")
+                print("The password is correct")
+                new_ramz = input("Do you want to change the password for more security?(yes/no) ")
                 if new_ramz == "yes":
-                    self.passw = hash(input("ramz jadid vRED KON"))
-                    print("ramz avaz shod")
+                    self.passw = hash(input("Enter the new password: "))
+                    print("Password changed")
                     break
                 elif new_ramz == "no":
                     break
             else:
-                print("ramz eshtabah dobre talah kon")
+                print("Wrong password. Try again")
 
-    def list_items(self):
-        f = open("demofile2.txt", "r")
+    def list_items(self,MainList):
+        f = open("Products.txt", "r")
         i = 1
         for line in f:
             date = line.split()
-            self.list1.append(date)
+            MainList.append(date)
             print(str(i) + " " + line)
             i = i + 1
-        print(self.list1)
+        if len(MainList) == 0:
+            print("There is no case")
 
-    def add_item(self):
-        print(self.list1)
+    def add_item(self,MainList):
         ans = "yes"
         while ans == "yes":
-            barcode = input("lotfan barcod vared konid")
-            for i in self.list1:
-                if barcode == i[0] and ans=="yes":
-                    num_item = int(input("kalaee k mikhay vared koni mojode che tedadi mikhay ezafe koni?"))
-                    i[3] = int(i[3]) + num_item
-                    print(i)
-                    print(
-                        "kala ezafe shod" + str(i[0]) + " " + str(i[1]) + " " + str(i[2]) + " " + str(i[3]) + " " + str(
-                            i[4]))
-                    ans = input("Do you want to import another product?(yes/no")
-                    break
+            barcode = input("Please enter the barcode")
+            name = input("Please enter the name: ")
+            brand = input("Please enter the brand: ")
+            count = input("Please count : ")
+            price = input("Please Price the brand: ")
+            f = open("Products.txt", "a")
+            line1 = barcode + " " + name + " " + brand + " " + count + " " + price
+            print("Product added : " + line1)
+            MainList.append(line1.split())
+            f.writelines( barcode + " " + name + " " + brand + " " + count + " " + price )
+            ans = input("Do you want to import another product?(yes/no)")
+            f.close()
 
-                elif ans == "yes":
-                    name = input("lotfan name vared konid")
-                    brand = input("lotfan brand vared konid")
-                    tedad = input("lotfan tedad vared konid")
-                    gheymat = input("lotfan gheymat vared konid")
-                    f = open("demofile2.txt", "a")
-                    line1 = barcode + " " + name + " " + brand + " " + tedad + " " + gheymat
-                    print("kala ezafe shod=  " + line1)
-                    self.list1.append(line1.split())
-                    f.writelines("\n" + barcode + " " + name + " " + brand + " " + tedad + " " + gheymat + "\n")
-                    ans = input("Do you want to import another product?(yes/no)")
-                    f.close()
+    def update_item(self,MainList):
+        ans = "yes"
+        while ans == "yes":
+            f = open("Products.txt", "r")
+            for line in f:
+                date = line.split()
+                MainList.append(date)
+            barcode = input("Please enter the barcode")
+            f = open("Products.txt", "r")
+            for line in f:
+                date = line.split()
+                MainList.append(date)
+            for i in MainList:
+                if barcode == i[0]:
+                    numitem = int(input("The product is available. How many should be added? "))
+                    i[3] = int(i[3]) + numitem
+                    print("Product added" + str(i[0]) + " " + str(i[1]) + " " + str(i[2]) + " " + str(i[3]) + " " + str(
+                        i[4]))
+                    break
+            ans = input("Do you want to import another product?(yes/no)")
 
     def factor(self):
         pass
