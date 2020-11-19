7b541ef765998f798e75f15b7263a6831b6bd1be
rhazare <zare.rhaa@gmail.com>
2020-11-20 03:29:37 +0330

    GUI admin: method logging admin, chk password, change password, changed, add product, adding

--- rha_admin_mixaidamlihe.py
+++ rha_admin_mixaidamlihe.py
@@ -139,17 +139,54 @@ class Admin:
         logger.info("Password changed")
         messagebox.showinfo("change Password", "Password changed successfully")
 
-    def add_new_product(self, product_name, brand, barcode, price, stock):
+    def add_new_product(self):
         """admin can add new product to the list of products and updates the entrepot"""
+        
+        addproduct = StoreWindow()
+        addproduct.rowconfigure(0, minsize=800, weight=1)
+        addproduct.columnconfigure(1, minsize=800, weight=1)
+        taskbar_frame = Frame(addproduct, relief=RAISED, bd=2, bg='grey')
+        btn_add = Button(taskbar_frame, text='New Product', bg='VioletRed4', command=self.add_new_product()).grid(
+            row=0, column=0, sticky="ew", padx=5, pady=5)
+        btn_invoice = Button(taskbar_frame, text='Invoices', bg='VioletRed4', command=self.show_invoices()).grid(
+            row=1, column=0, sticky="ew", padx=5, pady=5)
+        btn_change_password = Button(taskbar_frame, text='Change Password', bg='VioletRed4',
+                                     command=self.change_info()).grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        btn_charge = Button(taskbar_frame, text='Charge Product', bg='VioletRed4',
+                            command=self.charge_stock_by_admin()).grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=addproduct.quit).grid(row=10, column=0,
+                                                                                              sticky="ew", padx=5)
+        taskbar_frame.grid(row=0, column=0, sticky="ns")
+
+        fr_main = Frame(addproduct, relief=RAISED, bd=1)
+        self.product_name = Entry(fr_main, width=30)
+        self.brand = Entry(fr_main, width=30)
+        self.barcode = Entry(fr_main, width=30)
+        self.price = Entry(fr_main, width=30)
+        self.stock = Entry(fr_main, width=30)
+        add_btn = Button(fr_main, text='Add', command=self.adding()).grid(row=7, column=1, padx=100, pady=6)
+        lbl_product_name = ttk.Label(fr_main, text="product name : ").grid(row=2, column=0)
+        lbl_brand = ttk.Label(fr_main, text="brand : ").grid(row=3, column=0)
+        lbl_barcode = ttk.Label(fr_main, text="barcode : ").grid(row=4, column=0)
+        lbl_price = ttk.Label(fr_main, text="price : ").grid(row=5, column=0)
+        lbl_stock = ttk.Label(fr_main, text="stock : ").grid(row=6, column=0)
+        self.product_name.grid(row=2, column=1, sticky=W)
+        self.brand.grid(row=3, column=1, sticky=W)
+        self.barcode.grid(row=4, column=1, sticky=W)
+        self.price.grid(row=5, column=1, sticky=W)
+        self.stock.grid(row=6, column=1, sticky=W)
+
+    def adding(self):
         with open('product.csv', 'a', newline='') as csvpr:
             fieldnames = ['product name', 'brand', 'barcode', 'price', 'stock']
             writer = csv.DictWriter(csvpr, fieldnames=fieldnames)
-            writer.writerow({'product name': product_name,
-                             'brand': brand,
-                             'barcode': barcode,
-                             'price': price,
-                             'stock': stock})
-            ###in main:log.info:"new product added"
+            writer.writerow({'product name': self.product_name,
+                             'brand': self.brand,
+                             'barcode': self.barcode,
+                             'price': self.price,
+                             'stock': self.stock})
+            logger.info("new product added")
+            messagebox.showinfo('Add product','new product added')
 
     def show_invoices(self):
         """admin can see the previous invoices by this method"""
