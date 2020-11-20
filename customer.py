557bba39617c854798044fcd11fcc622008b5ee4
aidayg24 <aidarostamee221bb@gmail.com>
2020-11-20 15:01:45 +0330

    aida:the final customer module with buy and remove and invoice methods! upload:990830

--- customer.py
+++ customer.py
@@ -1,7 +1,9 @@
+import csv
+
+
 class Customer:
-    def __init__(self, basket, invoice, final_price):
+    def __init__(self, final_price=0, basket={}):
         self.basket = basket
-        self.invoice = invoice
         self.price = final_price
 
     @staticmethod
@@ -18,15 +20,49 @@ class Customer:
             else:
                 return "this product does not exist"
 
-    def buy(self, basket):
+    def buy(self, barcode, number):  # number is the amount of product that customer wants
         """the customer selects an item to buy and add that to the basket """
-        pass
+        file = open("product.csv", 'r')
+        for line in file.readlines():
+            data = line.strip().split(",")
+            if data[2] == barcode:
+                the_price_of_one = int(data[3])
+                self.basket[barcode] = ("category: " + data[0] + " brand: " + data[1] +
+                                        " barcode: " + data[2] + " price of one: " + data[3] +
+                                        " total: " + str(the_price_of_one * number))
+                self.price += (the_price_of_one * number)
 
-    def remove(self, basket):
-        """the customer can remove an item from the basket before he or she get the final invoice"""
-        pass
+    def remove(self, barcode):
+        """the customer can remove an item from the basket
+         before he or she get the final invoice. this function can not
+         edit the number of the product that customer buy but can remove that
+         item from basket"""
+        for item in self.basket.keys():
+            if item == barcode:
+                del self.basket[barcode]
 
-    def the_invoice(self, invoice, price):
-        """this method records the final items in basket and gives an invoice and the price to
+    def add_to_invoice(self):
+        """this method records the final items in basket
+        and gives an invoice and the price to
         customer and updates the entrepot """
-        pass
+        with open('invoice.csv', 'a', newline='') as invoice:
+            fieldnames = ['category', 'brand', 'barcode', 'price of one product', 'hole price']
+            writer = csv.DictWriter(invoice, fieldnames=fieldnames)
+            for item in self.basket.keys():
+                new_data = str(self.basket[item]).strip().split(" ")
+                writer.writerow({'category': new_data[1],
+                                 'brand': new_data[3],
+                                 'barcode': new_data[5],
+                                 'price of one product': new_data[7],
+                                 'hole price': new_data[9]})
+            writer.writerow({'category': "\n",
+                             'brand': "\n",
+                             'barcode': "\n",
+                             'price of one product': "\n",
+                             'hole price': "\n"})
+
+    def show_invoice_to_customer(self):
+        """this function show the final invoice to customer"""
+        for item in self.basket.keys():
+            print(self.basket[item])
+
