1908d49ee6eece850d334765bdfbc223cdcb2b8e
aidayg24 <aidarostamee221bb@gmail.com>
2020-11-20 16:24:11 +0330

    aida added comments! 990830

--- admin-malihe.py
+++ admin-malihe.py
@@ -1,14 +1,21 @@
+###***aida's comments
+
 class Admin:
 
     def __init__(self):
         self.passw = hash(13741387)
+        ###***dont use this method of hash . this will gives you alot of errors! use hashlib
+        ###***this library is better for hashing. read my aida_admin code I use this in my code.
 
-    def check_pass(self, ramz=None):
+    def check_pass(self, ramz=None):###*** please replace "ramz" with "password" and do not use fingilish in your code!
+        ###*** this while code is a great idea we can use this and it give admin the oppertunaty to
+        ###***try several times for give the password to system!
         while self.passw != ramz:
-            ramz = hash(int(input("enter your password: ")))
+            ramz = hash(int(input("enter your password: "))) ###***again the hash is not ok!
             if self.passw == ramz:
                 print("The password is correct")
                 new_ramz = input("Do you want to change the password for more security?(yes/no) ")
+                ###*** you could design an other method for change informations it would be better in my oppinion
                 if new_ramz == "yes":
                     self.passw = hash(input("Enter the new password: "))
                     print("Password changed")
@@ -20,9 +27,10 @@ class Admin:
 
     def list_items(self,MainList):
         f = open("Products.txt", "r")
+        ###*** I think working with txt files in this case is not the best idea.I used csv in my code
         i = 1
         for line in f:
-            date = line.split()
+            date = line.split()###*** it would be better to use .strip().split() in this case
             MainList.append(date)
             print(str(i) + " " + line)
             i = i + 1
@@ -31,6 +39,7 @@ class Admin:
 
     def add_item(self,MainList):
         ans = "yes"
+        ###***what is ans?? you did not have this before in your code!
         while ans == "yes":
             barcode = input("Please enter the barcode")
             name = input("Please enter the name: ")
@@ -40,13 +49,17 @@ class Admin:
             f = open("Products.txt", "a")
             line1 = barcode + " " + name + " " + brand + " " + count + " " + price
             print("Product added : " + line1)
+            ###***why you print this?there is no need for this
             MainList.append(line1.split())
             f.writelines( barcode + " " + name + " " + brand + " " + count + " " + price )
+            ###*** you could jast write f.write(line1)
             ans = input("Do you want to import another product?(yes/no)")
+            ###***its somehow confusing you could give ans as an arguman to function
             f.close()
 
     def update_item(self,MainList):
         ans = "yes"
+        ###***again same comment for ans!
         while ans == "yes":
             f = open("Products.txt", "r")
             for line in f:
