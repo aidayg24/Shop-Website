7707d37d68994e520bf58d499c68caea953f7380
aidayg24 <aidarostamee221bb@gmail.com>
2020-11-20 16:01:35 +0330

    aida added comments! 990830

--- basketform.py
+++ basketform.py
@@ -4,6 +4,6 @@ number=input() #entry
 '''name , barand , price for one --> read from file'''
 price_of_all= int(number)*int('price for one')
 basket[barcode1]=('name,brand',number,'price for one','price of all')
-
+###***aida:I add the thing taht you wanted to customer module!
 
 
