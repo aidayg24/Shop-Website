03c8adecac2da01143c2a58d7619fed5c9edf073
rhazare <zare.rhaa@gmail.com>
2020-11-21 16:25:40 +0330

    comment runner,customer done!

--- customer.py
+++ customer.py
@@ -1,51 +1,57 @@
-import csv
-from tkinter import ttk
-from tkinter import *
-from tkinter import messagebox
-import hashlib
-import logging
-from admin import Admin
-admin=Admin()
+import csv # importing csv
+from tkinter import ttk #importing ttk moudls
+from tkinter import * # importing tkinter library
+from tkinter import messagebox #importing messagebox moudls
+import logging #importing tkinter library for log information
+from admin import Admin #import class Admin from admin moudle
+
+admin=Admin()# make an object from class admin
+
+#Define the data logging format:
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(filename="logfile.log",
                     level=logging.DEBUG,
                     format=LOG_FORMAT,
                     filemode='a')
-
 logger = logging.getLogger()
 
-class Customer:
+
+class Customer: #Define the customer class to organize the client's tasks and authority
     def __init__(self):
+        #for save information use tuple cuse tuple is a collection which is ordered and unchangeable.
         self.basket= {'info':('name','brand','barcode','number','price for one','hole price','totall sum')}
 
-    def about(self):
+    def about(self):# This function displays information about the application
         messagebox.showinfo("about maktab store",
                             "Project 4: Store Accounting\nprogramming by:\ntahere zare(raha),aida rostami,malihe mirzaii")
 
     def add_to_basket(self):
-        addtobaskt = Toplevel()
+    #A window will open for the customer to enter the product and brand name and the number she wants to make a purchase
+        addtobaskt = Toplevel() # making a top window
+
         addtobaskt.menubar = Menu(addtobaskt)
         addtobaskt.helpmenu = Menu(addtobaskt.menubar, tearoff=0)
         addtobaskt.helpmenu.add_command(label="About", command=self.about)
         addtobaskt.menubar.add_cascade(label="Help", menu=addtobaskt.helpmenu)
         addtobaskt.config(menu=addtobaskt.menubar)  # display the menu
 
+        # Configure window rows and column
         addtobaskt.rowconfigure(0, minsize=800, weight=1)
         addtobaskt.columnconfigure(1, minsize=800, weight=1)
+
+        # Definition of taskbar:
         taskbar_frame = Frame(addtobaskt, relief=RAISED, bd=2, bg='grey')
-        btn_admin = Button(taskbar_frame, text='Admin', bg='firebrick4', command=admin.login).grid(row=0, column=0,
-                                                                                                   sticky="ew", padx=5,
-                                                                                                   pady=5)
-        btn_cart = Button(taskbar_frame, text='basket', bg='VioletRed4',command=self.showbasket).grid(row=2, column=0, sticky="ew", padx=5,
-                                                                            pady=5)
-        btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=addtobaskt.quit).grid(row=10, column=0, sticky="ew",
-                                                                                          padx=5)
+        btn_admin = Button(taskbar_frame, text='Admin', bg='firebrick4', command=admin.login).grid(row=0, column=0,sticky="ew", padx=5,pady=5)
+        btn_bsktet = Button(taskbar_frame, text='basket', bg='VioletRed4',command=self.showbasket).grid(row=2, column=0, sticky="ew", padx=5,pady=5)
+        btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=addtobaskt.quit).grid(row=10, column=0, sticky="ew",padx=5)
         taskbar_frame.grid(row=0, column=0, sticky="ns")
 
+        # Define the main frame:
         fr_main = Frame(addtobaskt, relief=RAISED, bd=1)
-        self.product_name = Entry(fr_main, width=30)
+        #To buy name, brand, number is entered:
+        self.product_name = Entry(fr_main, width=30) #Entry == input
         self.brand = Entry(fr_main, width=30)
-        self.number = Spinbox(fr_main, from_=0, to=100)
+        self.number = Spinbox(fr_main, from_=0, to=100) #give a number in range(0,100)
         add_btn = Button(fr_main, text='Add to basket', command=self.added).grid(row=7, column=1, padx=100, pady=6)
         lbl_product_name = ttk.Label(fr_main, text="product name : ").grid(row=2, column=0)
         lbl_brand = ttk.Label(fr_main, text="brand : ").grid(row=3, column=0)
@@ -53,22 +59,22 @@ class Customer:
         self.brand.grid(row=3, column=1, sticky=W)
         self.number.grid(row=4, column=1, sticky=W)
         fr_main.grid(row=0, column=1, sticky="nsew")
-        addtobaskt.mainloop()
+
+        addtobaskt.mainloop() # creating a loop for the main window to store the changes
 
     def added(self):
-        self.price=0
-        barcode=None
+        self.price=0 #Total purchase total
+        barcode=None #Defined for when the barcode was not found
+
+        #Using the name and brand, we find the barcode of the product:
         file = open("product.csv", 'r')
         for line in file.readlines():
             data = line.strip().split(",")
             if self.product_name.get().lower().strip() == data[0] and self.brand.get().lower().strip() == data[1]:
                 barcode=data[2]
-        file = open("product.csv", 'r')
-        for line in file.readlines():
-            data = line.strip().split(",")
-            if data[2] == barcode:
-                if int(self.number.get().strip()) < int(data[4]):
+                if int(self.number.get().strip()) < int(data[4]): #Check inventory number
                     the_price_of_one = int(data[3])
+                    #We store the product information with the barcode found in the basket
                     self.basket[barcode] = (data[0],data[1], data[2],int(self.number.get().strip()),data[3],the_price_of_one * int(self.number.get().strip()),None)
                     self.price += the_price_of_one * int(self.number.get().strip())
                     messagebox.showinfo('Buy','Product added to your basket')
@@ -77,28 +83,32 @@ class Customer:
                     messagebox.showerror('Buy', 'The amount you want is more than the inventory')
                     logger.error('Unsuccessful attempt to buy goods : Insufficient inventory')
 
-
     def showbasket(self):
-        baskett=Toplevel()
+    #A window will open to display the customer invoice as a table and finalize it if desired
+        baskett=Toplevel() # making a top window
+
         baskett.menubar = Menu(baskett)
         baskett.helpmenu = Menu(baskett.menubar, tearoff=0)
         baskett.helpmenu.add_command(label="About", command=self.about)
         baskett.menubar.add_cascade(label="Help", menu=baskett.helpmenu)
         baskett.config(menu=baskett.menubar)  # display the menu
 
+        # Configure window rows and column
         baskett.rowconfigure(0, minsize=800, weight=1)
         baskett.columnconfigure(1, minsize=800, weight=1)
+
+        # Definition of taskbar:
         taskbar_frame = Frame(baskett, relief=RAISED, bd=2, bg='grey')
-        btn_admin = Button(taskbar_frame, text='Admin', bg='firebrick4', command=admin.login).grid(row=0, column=0,
-                                                                                                   sticky="ew", padx=5,
-                                                                                                   pady=5)
-        btn_cart = Button(taskbar_frame, text='basket', bg='VioletRed4',command=self.showbasket).grid(row=2, column=0, sticky="ew", padx=5,
-                                                                            pady=5)
-        btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=baskett.quit).grid(row=10, column=0, sticky="ew",
-                                                                                          padx=5)
+        btn_admin = Button(taskbar_frame, text='Admin', bg='firebrick4', command=admin.login).grid(row=0, column=0,sticky="ew", padx=5,pady=5)
+        btn_cart = Button(taskbar_frame, text='basket', bg='VioletRed4',command=self.showbasket).grid(row=2, column=0, sticky="ew", padx=5,pady=5)
+        btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=baskett.quit).grid(row=10, column=0, sticky="ew",padx=5)
         taskbar_frame.grid(row=0, column=0, sticky="ns")
 
+        # Define the main frame:
         fr_main = Frame(baskett, relief=RAISED, bd=1)
+
+        #The customer invoice table is shown to her
+        #Table top:
         producnamebrand = Label(fr_main, text='info',bg='tomato4')
         price = Label(fr_main, text='price of one',bg='tomato4')
         number = Label(fr_main, text='counter',bg='tomato4')
@@ -107,6 +117,8 @@ class Customer:
         number.grid(row=0, column=1, sticky="w", padx=5, pady=5)
         price.grid(row=0, column=2, sticky="w", padx=5, pady=5)
         sum.grid(row=0, column=3, sticky="w", padx=5, pady=5)
+
+        #Table information:
         row=2
         self.totallsum=0
         for product in self.basket.values():
@@ -122,16 +134,25 @@ class Customer:
                 price.grid(row=row, column=2, sticky="w", padx=5, pady=5)
                 sum.grid(row=row, column=3, sticky="w", padx=5, pady=5)
             row += 1
+
+        #The final addition to the shopping cart:
         totall= Label(fr_main,text='totall sum = '+str(self.totallsum))
         totall.grid(row=row, column=3, sticky="w", padx=5, pady=5)
+
+        #Purchase registration:
         buy_btn = Button(fr_main, text="Click if you want your purchase to be final", bg='red4', command=self.buy)
         buy_btn.grid(row=row+1,sticky="w", padx=5, pady=5)
+
         fr_main.grid(row=0, column=1, sticky="nsew")
-        baskett.mainloop()
+
+        baskett.mainloop() # creating a loop for the main window to store the changes
 
     def buy(self):
+        #This function records the customer's purchase:
         messagebox.showinfo('Buy','Your purchase has been registered')
         logger.info('A purchase was made')
+
+        #Records the purchase information in the invoice file
         with open('invoice.csv', 'a', newline='') as invoice:
             fieldnames = ['category', 'brand', 'barcode','number', 'price of one product', 'hole price','totall sum']
             writer = csv.DictWriter(invoice, fieldnames=fieldnames)
@@ -145,6 +166,7 @@ class Customer:
                                  'totall sum':item[6]})
             writer.writerow({'totall sum': self.totallsum})
 
+        #Updates inventory:
         for item in self.basket.values():
             file = open("product.csv", 'r')
             file_data = file.readlines()
--- runner.py
+++ runner.py
@@ -1,22 +1,23 @@
 from tkinter import *  # importing tkinter library
-from tkinter import messagebox #importing messagebox moudls
-from admin import Admin #import class Admin from admin moudle
-from customer import Customer#import class Customer from customer moudle
+from tkinter import messagebox  # importing messagebox moudls
+from admin import Admin  # import class Admin from admin moudle
+from customer import Customer  # import class Customer from customer moudle
 
-admin=Admin() #make an object from class admin
-customer=Customer() #make an object from class customer
+admin = Admin()  # make an object from class admin
+customer = Customer()  # make an object from class customer
 
-def about(): #This function displays information about the application
+
+def about():  # This function displays information about the application
     messagebox.showinfo("about maktab store",
                         "Project 4: Store Accounting\nprogramming by:\ntahere zare(raha),aida rostami,malihe mirzaii")
 
 
 store = Tk()  # making a window for the home page of the shop
-store.title('maktab store') #In all TK() and Toplevel(), this is the title of the window
+store.title('maktab store')  # In all TK() and Toplevel(), this is the title of the window
 
-store.minsize(500, 500) #Window size restrictions
-store.maxsize(1600, 1200) #Window size restrictions
-store.geometry('800x600') #Window size restrictions
+store.minsize(500, 500)  # Window size restrictions
+store.maxsize(1600, 1200)  # Window size restrictions
+store.geometry('800x600')  # Window size restrictions
 
 store.menubar = Menu(store)
 store.helpmenu = Menu(store.menubar, tearoff=0)
@@ -24,39 +25,42 @@ store.helpmenu.add_command(label="About", command=about)
 store.menubar.add_cascade(label="Help", menu=store.helpmenu)
 store.config(menu=store.menubar)  # display the help menu
 
-#Configure window rows and column
+# Configure window rows and column
 store.rowconfigure(0, minsize=800, weight=1)
 store.columnconfigure(1, minsize=800, weight=1)
 
-#Definition of taskbar:
+# Definition of taskbar:
 taskbar_frame = Frame(store, relief=RAISED, bd=2, bg='grey')
 btn_admin = Button(taskbar_frame, text='Admin', bg='firebrick4', command=admin.login).grid(row=0, column=0,
-                                                                                            sticky="ew", padx=5, pady=5)
-btn_basket = Button(taskbar_frame, text='basket', bg='VioletRed4',command=customer.showbasket).grid(row=2, column=0,sticky="ew", padx=5,pady=5)
+                                                                                           sticky="ew", padx=5, pady=5)
+btn_basket = Button(taskbar_frame, text='basket', bg='VioletRed4', command=customer.showbasket).grid(row=2, column=0,
+                                                                                                     sticky="ew",
+                                                                                                     padx=5, pady=5)
 btn_exit = Button(taskbar_frame, text="Exit", bg='red4', command=store.quit).grid(row=10, column=0, sticky="ew", padx=5)
 taskbar_frame.grid(row=0, column=0, sticky="ns")
 
-#Define the main frame:
+# Define the main frame:
 fr_main = Frame(store, relief=RAISED, bd=1)
 lbl_welcome = Label(fr_main, text='Maktab store\n\n', fg='black', font=('Garamond', 20))
 
-#By default, the customer is recognized at the beginning of entering the customer's store
+# By default, the customer is recognized at the beginning of entering the customer's store
 file = open("product.csv", 'r')
-row=3
-lines=file.readlines()
-if len(lines)>1 : #If a product is defined
+row = 3
+lines = file.readlines()
+if len(lines) > 1:  # If a product is defined
     for line in lines:
         data = line.strip().split(",")
-        #The customer is shown only the name of the product, brand and price:
-        nameproduc=Label(fr_main,text=data[0])
-        brand=Label(fr_main,text=data[1])
-        price=Label(fr_main,text=data[3])
-        nameproduc.grid(row=row,column=0, sticky="w", padx=5, pady=5)
-        brand.grid(row=row,column=1, sticky="w", padx=5, pady=5)
-        price.grid(row=row,column=2, sticky="w", padx=5, pady=5)
-        row+=1
+        # The customer is shown only the name of the product, brand and price:
+        nameproduc = Label(fr_main, text=data[0])
+        brand = Label(fr_main, text=data[1])
+        price = Label(fr_main, text=data[3])
+        nameproduc.grid(row=row, column=0, sticky="w", padx=5, pady=5)
+        brand.grid(row=row, column=1, sticky="w", padx=5, pady=5)
+        price.grid(row=row, column=2, sticky="w", padx=5, pady=5)
+        row += 1
 
-btn_buy=Button(fr_main, text="Buy something", bg='red4', command=customer.add_to_basket).grid(row=row, column=0, sticky="ew", padx=5)
+btn_buy = Button(fr_main, text="Buy something", bg='red4', command=customer.add_to_basket).grid(row=row, column=0,
+                                                                                                sticky="ew", padx=5)
 file.close()
 lbl_welcome.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
 fr_main.grid(row=0, column=1, sticky="nsew")
